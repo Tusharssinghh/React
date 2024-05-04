eeca41dd314227780c1a5e5fb025c42ecab9829c
Tusharssinghh <Tusharsinghmodi@gmail.com>
2024-05-04 10:02:17 +0530

    Using "useRef"

--- 05Making_password/src/App.jsx
+++ 05Making_password/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useCallback, useEffect } from 'react'
 
 import './App.css'
+import { useRef } from 'react';
 
 function App() {
   
@@ -12,6 +13,8 @@ function App() {
 
   const [password, setpassword] = useState("");
 
+  const passwordRef = useRef(null);
+
   const passwordGenerator = useCallback(()=>{
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -21,17 +24,23 @@ function App() {
     if(characterAllowed) str += "`~!@#$%^&*()_+{}[]\|."
 
     for(let i=0; i<length; i++){
-      let char = Math.floor(Math.random() * str.length() + 1)
-      pass = str.charAt(char)
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
     }
     setpassword(pass)
   }, [length, numberAllowed, characterAllowed, setpassword])
+
+  const copypasswordtoclipboard = useCallback(() => {
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,4)
+    window.navigator.clipboard.writeText(password)
+  }, [password])
 
   //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   // useEffect hook takes callback function and array
   useEffect(() => {
     passwordGenerator()
-  }, [length, characterAllowed, numberAllowed, passwordGenerator])
+  }, [length, characterAllowed, numberAllowed, passwordGenerator]);
 
   return (
     <>
@@ -42,15 +51,20 @@ function App() {
           value = {password} 
           className = "outline-none w-full py-1 px-3 " 
           placeholder= "password" 
-          readOnly/>
+          readOnly
+          ref = {passwordRef}
+          />
 
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">Copy</button>
+          <button 
+          onClick = {copypasswordtoclipboard}
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">Copy
+          </button>
 
         </div>
       {/* adding the number and character */}
       
           <div className= "flex text-sm gap-x-2">
-            <div claaaName = "flex items-center gap-x-1">
+            <div className = "flex items-center gap-x-1">
               <input 
               type="range" 
               min = {6}
